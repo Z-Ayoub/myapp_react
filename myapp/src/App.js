55c19e4f9eea98f25f67d45bb73c242435f4be69
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-28 18:56:53 +0200

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,20 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 const App = () => {
-  let myVar = 'Hello world';
-  
-  const onClickHandler = () => {
-   console.log('clicked');
-   myVar = 'Goodbye';
-   console.log(myVar);
-  }
-  
-  return(
-   <div className="App">
-    <button onClick={onClickHandler}>Click Me</button>
-    {myVar}
-    </div>
-  )};
+
+ /* Déclare une nouvelle variable d'état, que l'on va appeler « myVar » ,
+  Nous allons créer un état appelé «thème» à l'aide de useState.
+  useState renverra la valeur actuelle du "myVar" et une fonction pour mettre à jour le "myVar".
+ */
+const [myVar, setMyVar] = useState('Hello World');
+
+const onClickHandler = () => {
+
+  // on peut utiliser cette méthode
+  // setMyVar('Hello World updated');
+
+  // ou bien cette méthode pour mettre à jour << myVar >> lorsque l’utilisateur clique sur le bouton
+ setMyVar((prevState) => {
+   return prevState + 'updated';
+ })
+}
+
+return(
+ <div className="App">
+  <button onClick={onClickHandler}>Click Me</button>
+	{myVar}
+  </div>
+)};
 
 export default App;
