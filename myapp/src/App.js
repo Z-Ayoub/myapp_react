7e95ee5beb10d9b20e7cbf383b633f947756b3d2
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-28 18:30:27 +0200

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  let myVar = 'Hello world';
+  
+  const onClickHandler = () => {
+   console.log('clicked');
+   myVar = 'Goodbye';
+   console.log(myVar);
+  }
+  
+  return(
+   <div className="App">
+    <button onClick={onClickHandler}>Click Me</button>
+    {myVar}
     </div>
-  );
-}
+  )};
 
 export default App;
