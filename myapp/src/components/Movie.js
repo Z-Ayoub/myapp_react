0080426d077c7bb145b8dcd6ad13251679f7cccb
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-28 19:59:04 +0200

    css1

--- myapp/src/components/Movie.js
+++ myapp/src/components/Movie.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import toto from './Movie.module.css'
 
 const Movie = (props) => {
     return (
-        <div>
+        <div className={toto.divv}>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
             <p>{props.director}</p>
