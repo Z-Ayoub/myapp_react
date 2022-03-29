e71adadb0cb17e3102d7c6d35e7e75e5795a2bd7
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-29 11:39:13 +0200

    Installer React_Route_dom@6

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {useEffect, useState} from 'react';
 import Test from './components/Test';
 import Movie from './components/Movie';
+import { Link } from "react-router-dom";
 
 const App = () => {
 
